d343e789f24f93f607281af4455e672298784e42
lidan <lidangz@yeah.net>
2016-06-30 16:28:41 +0800

    delete jd

--- dbutils.js
+++ dbutils.js
@@ -1,22 +1,11 @@
 var mongostr = {                 // local machine
 		"hostname":"localhost", 
 		"port":27017, 
-		"username":"test", 
-		"password":"1234",  
+		"username":"", 
+		"password":"",  
 		"name":"", 
 		"db":"course" 
 }
-
-if(process.env.PORT){             // jd cloud
-	mongostr = { 
-		"hostname":"10.0.31.21", 
-		"port":27017, 
-		"username":"H8a97QJH", 
-		"password":"0R0m5E9j9N32",  
-		"name":"", 
-		"db":"lidangzm_mongo_1xv73077" 
-	}
-} 
 
 if(process.env.OPENSHIFT_NODEJS_PORT){     // OPENshift
 	mongostr = { 
